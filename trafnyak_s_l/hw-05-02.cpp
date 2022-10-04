16bc7261a0a19eeb2298a8545dca5aeecd8780a6
Semelion <senya2708@gmail.com>
2022-10-04 18:12:48 +0300

    home work for 04.10

--- trafnyak_s_l/hw-05-02.cpp
+++ trafnyak_s_l/hw-05-02.cpp
@@ -1,34 +1,32 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cmath>
 
 #define epsilon 0.001
-#define delta 0.1
-#define a -1.0
+#define delta 0.05
+#define a 0.0
 #define b 1.0
 
-long factorial(long n){
-    long fact = 1;
-    for(int i = 1; i <= n; i++)
-        fact *= i;
-    return fact;
-}
-
 int main(){
-    double summ = 0;
-    long long i = 0;
+    double summ = 0.0;
+    long long i = 1;
 
     std::cout << " x | s(x) | f(x)\n";
 
     bool flag = true;
     while (flag){
         for(double x = a; x <= b; x += delta) {
-            double temp = std::pow(-1, i) * (std::pow(x,2*i) / factorial(2*i));
+            double temp = double(std::pow(x, i)) * std::sin(i * M_PI / 4.0);
             summ += temp;
+
+            double y = x * std::sin(M_PI / 4.0) / (1 - 2 * x * std::cos(M_PI / 4.0));
+            std::cout << x << " | " << summ << " | " << y << "\n";
+
             if(std::abs(temp) < epsilon){
                 flag = false;
                 break;
             }
-            std::cout << x << " | " << summ << " | " << std::cos(x) << "\n";
         }
         i++;
     }
---
+++ trafnyak_s_l/hw-05-03.cpp
@@ -0,0 +1,41 @@
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+
+#define epsilon 0.0001
+#define delta 0.1
+#define a 0.0
+#define b 1.0
+
+double factorial(long n){
+    double fact = 1;
+    for(int i = 1; i <= n; i++)
+        fact *= i;
+    return fact;
+}
+
+int main(){
+    double summ = 0.0;
+    long i = 0;
+
+    std::cout << " x | s(x) | f(x)\n";
+
+    bool flag = true;
+    while (flag){
+        for(double x = a; x <= b; x += delta) {
+            double temp = double(std::pow(2*x, i)) / factorial(i);
+            summ += temp;
+
+            double y = pow(M_E, 2*x);
+            std::cout << x << " | " << summ << " | " << y << "\n";
+
+            if(std::abs(temp) < epsilon){
+                flag = false;
+                break;
+            }
+        }
+        i++;
+    }
+    return 0;
+}
