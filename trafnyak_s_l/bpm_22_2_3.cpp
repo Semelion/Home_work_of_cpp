2b83b00c8ceef428f8f3c49f9c8e521eece4a907
Semelion <senya2708@gmail.com>
2022-12-22 13:11:58 +0200

    -

--- trafnyak_s_l/bpm_22_2_3.cpp
+++ trafnyak_s_l/bpm_22_2_3.cpp
@@ -11,16 +11,20 @@ int main() {
         if (input.size() % 2 == 1) {
             std::string temp1 = "";
             std::string temp2 = "";
-            if(output.size() > 1) {
-                for (int i = 0; i < output.size() / 2; i++) {
-                    temp1 += output[i];
-                    temp2 += output[output.size() - i - 1];
-                }
-                output = temp1 + input[input.size() / 2 + 1] + temp2;
-            } else{
-                if(output.size() == 0){
-                    output = input[input.size()-1];
+            if (output.size() > 1) {
+                if(input.size() > 1) {
+                    for (int i = 0; i < output.size() / 2; i++) {
+                        temp1 += output[i];
+                        temp2 += output[output.size() - i - 1];
+                    }
+                    output = temp1 + input[input.size() / 2 + 1] + temp2;
                 } else{
+                    output.insert(output.size()/2,input);
+                }
+            } else {
+                if (output.size() == 0) {
+                    output = input[input.size() - 1];
+                } else {
                     output = input[input.size() - 1] + output;
                 }
             }
@@ -34,16 +38,16 @@ int main() {
             std::string temp1 = "";
             std::string temp2 = "";
 
-            if(output.size() > 1) {
+            if (output.size() > 1) {
                 for (int i = 0; i < output.size() / 2; i++) {
                     temp1 += output[i];
                     temp2 += output[output.size() - i - 1];
                 }
-                output = temp1 + output[output.size() / 2 + 1] + input[input.size() / 2 + 1] + temp2;
-            } else{
-                if(output.size() == 0){
-                    output = input[input.size()-1];
-                } else{
+                output = temp1 + input[input.size() - 1] + output[output.size() / 2] + temp2;
+            } else {
+                if (output.size() == 0) {
+                    output = input[input.size() - 1];
+                } else {
                     output = input[input.size() - 1] + output;
                 }
             }
