52bfaec711eec02d5324e7438b70edeb4f8a30d0
Semelion <senya2708@gmail.com>
2023-01-14 19:03:33 +0200

    -

--- trafnyak_s_l/bpm_22_2_1.cpp
+++ trafnyak_s_l/bpm_22_2_1.cpp
@@ -9,7 +9,7 @@
 int main(){
     std::cout << "x\tsum\n";
 
-    for(double x = x_start; x <= x_end; x += delta){
+    for(double x = x_start; x < x_end + delta; x += delta){
         int i = 0;
         double element = std::pow(-1, i)*(std::pow(x, (2*i + 1)) / (2*i + 1));
         double sum = element;
